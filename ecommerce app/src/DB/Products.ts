6bf5fafcee44acb12a4bf86427816b226b391a03
Anurag Omprakash <Anurag.Omprakash@godigit.com>
2024-08-08 11:40:22 +0530

    implement static dummy data for DB

---
+++ ecommerce app/src/DB/Products.ts
@@ -0,0 +1,26 @@
+import Product from "../interface/Product";
+
+let Products: Product[] =  [
+    { id: 1, name: "Laptop", quantity: 30, cost: 1000 },
+    { id: 2, name: "Smartphone", quantity: 50, cost: 700 },
+    { id: 3, name: "Headphones", quantity: 45, cost: 150 },
+    { id: 4, name: "Keyboard", quantity: 25, cost: 70 },
+    { id: 5, name: "Mouse", quantity: 60, cost: 40 },
+    { id: 6, name: "Monitor", quantity: 20, cost: 200 },
+    { id: 7, name: "Tablet", quantity: 15, cost: 350 },
+    { id: 8, name: "Smartwatch", quantity: 40, cost: 250 },
+    { id: 9, name: "Camera", quantity: 10, cost: 500 },
+    { id: 10, name: "Printer", quantity: 13, cost: 120 },
+    { id: 11, name: "Router", quantity: 12, cost: 100 },
+    { id: 12, name: "Speaker", quantity: 35, cost: 90 },
+    { id: 13, name: "External Hard Drive", quantity: 18, cost: 80 },
+    { id: 14, name: "Gaming Console", quantity: 22, cost: 300 },
+    { id: 15, name: "E-reader", quantity: 9, cost: 130 },
+    { id: 16, name: "Fitness Tracker", quantity: 33, cost: 60 },
+    { id: 17, name: "Drone", quantity: 5, cost: 750 },
+    { id: 18, name: "VR Headset", quantity: 7, cost: 350 },
+    { id: 19, name: "Action Camera", quantity: 14, cost: 250 },
+    { id: 20, name: "Microphone", quantity: 21, cost: 85 }
+];
+
+export default Products;
